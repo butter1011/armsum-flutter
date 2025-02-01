eedc23b87e4f8f329e838b590889cd3ae5268217
Leopard <kanjirohonda@gmail.com>
2025-02-01 18:35:38 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/armsum/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.armsum
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/theme_provider.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider with ChangeNotifier {
+  bool _isDarkMode = false;
+  bool get isDarkMode => _isDarkMode;
+
+  ThemeProvider() {
+    _loadThemeFromPrefs();
+  }
+
+  void toggleTheme() {
+    _isDarkMode = !_isDarkMode;
+    _saveThemeToPrefs();
+    notifyListeners();
+  }
+
+  Future<void> _loadThemeFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDarkMode = prefs.getBool('isDarkMode') ?? false;
+    notifyListeners();
+  }
+
+  Future<void> _saveThemeToPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDarkMode', _isDarkMode);
+  }
+
+  ThemeData get themeData {
+    return _isDarkMode ? _darkTheme : _lightTheme;
+  }
+
+  final _lightTheme = ThemeData(
+    primarySwatch: Colors.blue,
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: Colors.white,
+    primaryColor: Colors.black,
+    buttonTheme: const ButtonThemeData(
+      buttonColor: Colors.black,
+    ),
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(color: Colors.black87),
+      bodyMedium: TextStyle(color: Colors.black87),
+    ),
+  );
+
+  final _darkTheme = ThemeData(
+    primarySwatch: Colors.blue,
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: Colors.grey[900],
+    primaryColor: Colors.white,
+    buttonTheme: ButtonThemeData(
+      buttonColor: Colors.white,
+    ),
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(color: Colors.white),
+      bodyMedium: TextStyle(color: Colors.white70),
+    ),
+  );
+}
